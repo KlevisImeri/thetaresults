9b901a1a66228f54254e1a07693d0a99672ae571
Klevis Imeri <klevisimeri11@gmail.com>
2025-10-06 21:53:37 +0200

    Added merge.js

---
+++ merge.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env bun
+import { $ } from "bun";
+
+const mergedName = "3:PredCart(100, true) -> Kind() | 2:PredCart(100, true) -> KInd()";
+
+
+const individualFolders = mergedName.split(' | ');
+
+const inputFiles = individualFolders.map(
+  (folder) => `./results/baseline/${folder}/*unreach-call.xml.bz2`
+);
+
+const outputFile = `./results/${mergedName}`;
+
+const command = [
+    "./benchexec/bin/table-generator",
+    ...inputFiles,
+    "-o",
+    outputFile
+];
+
+console.log("Input folders parsed:");
+console.log(individualFolders);
+console.log(command.join(" "));
+await $`${command}`;
