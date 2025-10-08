9ed261da00316df745d7eef4a0500110beaef86d
Klevis Imeri <klevisimeri11@gmail.com>
2025-10-08 08:14:14 +0200

    Add

--- merge.js
+++ merge.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 import { $ } from "bun";
 
-const mergedName = "4:PredCart(100, true, true) -> KInd() | 2:PredCart(100, true) -> KInd()";
+const mergedName = "4:PredCart(100, true, true) -> KInd() | 2:PredCart(100, true) -> KInd() | 1:Kind() | 1:PredCart()";
 
 
 const individualFolders = mergedName.split(' | ');
