5dddd326b9a624a0424861f7e520c988a287aefb
Klevis Imeri <klevisimeri11@gmail.com>
2025-11-02 01:37:08 +0100

    Add

--- merge.js
+++ merge.js
@@ -2,8 +2,8 @@
 import { $ } from "bun";
 
 // const mergedName = "4:PredCart(900, true, true) -> KInd() | 4:PredCart(100, true, true) -> KInd() | 4:PredCart(100) -> KInd().all | 4:PredCart() | 1:Kind()";
-// const mergedName = "4:PredCart(900, true, true) -> KInd() | 4:PredCart(900, true) -> KInd()";
-const mergedName = "4:PredCart(100, true, true) -> KInd() | 4:PredCart(100, true) -> KInd()";
+// const mergedName = "4:PredCart(900, true, true) -> KInd() | 4:PredCart(900, true) -> KInd() | 4:PredCart() | 1:Kind()";
+const mergedName = "4:PredCart(100, true, true) -> KInd() | 4:PredCart(100, true) -> KInd() | 4:PredCart() | 1:Kind()";
 
 
 const individualFolders = mergedName.split(' | ');
